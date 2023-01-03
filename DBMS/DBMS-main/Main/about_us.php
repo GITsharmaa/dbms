c9fcbf823ef227b7fd2119f8595b6021716a770e
GITsharmaa <119485879+GITsharmaa@users.noreply.github.com>
2023-01-03 15:00:28 +0530

    Add files via upload

---
+++ DBMS/DBMS-main/Main/about_us.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About Us</title>
+    <link rel="stylesheet" href="./CSS/styler.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Courgette&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Courgette&family=Merienda:wght@400;700&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Courgette&family=Merienda:wght@400;700&family=Yeon+Sung&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Sriracha&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <nav>
+        <div class="top-header">
+            <ul class="home_contact">
+                
+                    <a class="active" href="#home">Home</a>
+                    <a href="#news">Lost</a>
+                    <a href="#contact">Found</a>
+                    <a href="#about">AboutUs</a>
+                    <a href="#about">ContactUs</a>
+
+            </ul>
+        </div>
+        <div class="ravi">
+            <img class="img1" src="./images/raviiii.png" alt="">
+            </div>
+            <div class="icon">
+                <img class="ravii" src="./images/linkedinrrr.png" alt="">
+                <img class="ravii" src="./images/githubrrr.png" alt="">
+                <img class="ravii" src="./images/instagram rr.png" alt="">
+                
+            </div>
+
+            <div class="rajan">
+                <img class="img1" src="./images/rajannn.png" alt="">
+            </div>
+
+            <div class="icon">
+                <img class="rajann" src="./images/linkedinrrr.png" alt="">
+                <img class="rajann" src="./images/githubrrr.png" alt="">
+                <img class="rajann" src="./images/instagram rr.png" alt="">
+                
+            </div>
+
+        </div>
+    </nav>
+  </body>
+</html>
